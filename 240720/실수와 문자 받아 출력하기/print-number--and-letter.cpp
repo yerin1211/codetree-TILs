c8bc41d8ca37a44c3a14fb931b337afe29903928
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-20 14:45:06 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char c;
+    double a, b;
+    cin >> c >> a >> b;
+    cout << fixed;
+    cout.precision(2);
+    cout << c << endl << a << endl << b;
+    return 0;
+}

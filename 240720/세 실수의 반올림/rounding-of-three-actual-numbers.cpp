6124629a8bf0d3d3b6162d35e208ba5bb977cd59
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-20 14:41:31 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240720/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+    cin >> a >> b >> c;
+    cout << fixed;
+    cout.precision(3);
+    cout << a << endl << b << endl << c;
+    return 0;
+}

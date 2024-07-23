b0ea9fbb05eff7a983bd2830f1eeb43945283220
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-23 10:43:04 +0900

    Title: 시력 검사, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/시력 검사/eye-test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    cin >> a >> b;
+    if(a >= 1.0 && b >= 1.0) cout << "High";
+    else if(a >= 0.5 && b >= 0.5) cout << "Middle";
+    else cout << "Low";
+    return 0;
+}

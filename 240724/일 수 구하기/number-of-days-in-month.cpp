a8acbd59092c7fbfcbf97b975266d19df08904c2
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-24 23:34:22 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/일 수 구하기/number-of-days-in-month.cpp
+++ 240724/일 수 구하기/number-of-days-in-month.cpp
@@ -5,7 +5,7 @@ int main() {
     int n;
     cin >> n;
     if(n == 2) cout << 28;
-    else if(n % 2 && n <= 8 || !(n % 2) && n > 8) cout << 31;
+    else if(n % 2 && n <= 7 || !(n % 2) && n > 7) cout << 31;
     else cout << 30;
     return 0;
 }

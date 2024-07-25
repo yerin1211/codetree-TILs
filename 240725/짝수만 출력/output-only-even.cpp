24aea996d5562712437d78bc14cd5a25c809a346
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-25 23:59:34 +0900

    Title: 짝수만 출력, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    while(a <= b){
+        cout << a << ' ';
+        a += 2;
+    }
+    return 0;
+}

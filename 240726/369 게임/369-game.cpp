fe22e06d58c2e2d930fc3f6401e0b87471d1fd50
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-26 12:29:46 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240726/369 게임/369-game.cpp
+++ 240726/369 게임/369-game.cpp
@@ -2,10 +2,8 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    for(int i = 1; i <= n; i++){
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++)
         cout << ((i%3) && ((i%10)%3 || !(i%10)) && ((i/10)%3 || !(i/10)) ? i : 0) << ' ';
-    }
     return 0;
 }

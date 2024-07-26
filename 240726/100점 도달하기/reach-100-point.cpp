edbf07e0eb69d1fa4ed59d058ded6c200c1cfe9a
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-26 11:18:17 +0900

    Title: 100점 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240726/100점 도달하기/reach-100-point.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(;n <= 100; n++){
+        if(n >= 90) cout << "A ";
+        else if(n >= 80) cout << "B ";
+        else if(n >= 70) cout << "C ";
+        else if(n >= 60) cout << "D ";
+        else cout << "E ";
+    }
+    return 0;
+}

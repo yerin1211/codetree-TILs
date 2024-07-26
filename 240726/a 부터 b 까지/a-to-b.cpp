c3a4d6132cbe1a112f0a8ec75550aebd3bee7918
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-26 11:22:12 +0900

    Title: a 부터 b 까지, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/a 부터 b 까지/a-to-b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    while(a <= b){
+        cout << a << ' ';
+        if(a % 2) a *= 2;
+        else a += 3;
+    }
+    return 0;
+}

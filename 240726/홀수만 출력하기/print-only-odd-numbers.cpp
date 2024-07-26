e25d4099f69cf08bd2951b806db6b6a2ca2e49af
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-26 12:32:37 +0900

    Title: 홀수만 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        if(a % 2 && !(a % 3)) cout << a << '\n';
+    }
+    return 0;
+}

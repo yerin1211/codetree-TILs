6df78c320ea8165d62a890ad7780275b262d71e7
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-29 20:43:25 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240729/친근하지 않은 수/unfriendly-number.cpp
+++ 240729/친근하지 않은 수/unfriendly-number.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n, c = 0;
     cin >> n;
-    while(n--) if(n % 2 && n % 3 && n % 5) c++;
+    for(; n >= 1; n--) if(n % 2 && n % 3 && n % 5) c++;
     cout << c;
     return 0;
 }

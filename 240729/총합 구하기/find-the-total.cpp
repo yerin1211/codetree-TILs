9bb38310b8a68cf086017e75205a55ccd45950f3
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-29 20:36:46 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/총합 구하기/find-the-total.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, s = 0;
+    cin >> a >> b;
+    for(; a <= b; a++) if(!(a % 6) && a % 8) s += a;
+    cout << s;
+    return 0;
+}

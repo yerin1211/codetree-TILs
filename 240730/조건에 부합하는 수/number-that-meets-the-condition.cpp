244ef7073564c039582499a4bf63204b8033ac04
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-30 16:17:51 +0900

    Title: 조건에 부합하는 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a; cin >> a;
+    for(int i = 1; i <= a; i++) if( (i % 2 || !(i % 4)) && (i / 8) % 2 && (i % 7) >= 4 ) cout << i << ' ';
+    return 0;
+}

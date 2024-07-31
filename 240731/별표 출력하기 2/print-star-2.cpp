292f82ad14f82dbcc84c713215605e0b11bc26b8
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-07-31 14:16:06 +0900

    Title: 별표 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240731/별표 출력하기 2/print-star-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    while(n--){
+        for(int i = 0; i <= n; i++) cout << "* ";
+        cout << '\n';
+    }
+    return 0;
+}

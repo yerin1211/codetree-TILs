2f48f9c032513255e3954b1d8db3da782577dbb7
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-01 23:53:16 +0900

    Title: 행과 열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/행과 열/row-column.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    for(int i = 1; i <= a; i++){
+        for(int j = 1; j <= b; j++) cout << i * j << ' ';
+        cout << '\n';
+    }
+    return 0;
+}

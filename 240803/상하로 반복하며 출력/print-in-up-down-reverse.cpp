d8fb75f09a60bf000660907ea5df50859f6ecde7
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-03 23:59:34 +0900

    Title: 상하로 반복하며 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240803/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i, j;
+    cin >> n;
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= n; j++) cout << (j % 2 ? i : n - i + 1);
+        cout << '\n';
+    }
+    return 0;
+}

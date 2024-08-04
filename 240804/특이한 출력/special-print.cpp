cd236609e8a404ad71891d0c2f4db8b143796981
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-04 15:36:46 +0900

    Title: 특이한 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240804/특이한 출력/special-print.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i, j;
+    cin >> n;
+    for(i = 1; i <= n; i++) for(j = 1; j <= n; j++) cout << '(' << i << ", " << j << ')' << ((i + j) % 4 ? ' ' : '\n');
+    return 0;
+}

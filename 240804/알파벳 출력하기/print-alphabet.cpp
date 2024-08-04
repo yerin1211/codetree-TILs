a587518fafd06c39693d6d31dfb6fdeaa470f807
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-04 16:01:57 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240804/알파벳 출력하기/print-alphabet.cpp
+++ 240804/알파벳 출력하기/print-alphabet.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main() {
-    int n, i, j, c = 65;
+    int n, i, j;
+    char c = 'A';
     cin >> n;
-    for(i = 1; i <= n; i++) for(j = 1; j <= i; j++) cout << (char)c++ << (j - i ? "" : "\n");
+    for(i = 1; i <= n; i++){
+        for(j = 1; j <= i; j++){
+            cout << c++;
+            if(c > 'Z') c = 'A';
+        }
+        cout << '\n';
+    }
     return 0;
 }

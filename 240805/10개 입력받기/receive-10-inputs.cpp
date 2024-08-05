f15fa400947f2dd5f4e79be62897996f2758ab09
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-05 11:11:25 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240805/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, s = 0, i;
+    for(i = 0; i < 10; i++){
+        cin >> a;
+        if(!a) break;
+        s += a;
+    }
+    cout << fixed;
+    cout.precision(1);
+    cout << s << ' ' << (double)s / i;
+    return 0;
+}

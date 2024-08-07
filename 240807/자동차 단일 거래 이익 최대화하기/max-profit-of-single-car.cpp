94add557dcd9099a38912d5a5543d6a75014d080
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-07 15:12:30 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240807/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
+++ 240807/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 
 int main() {
-    int n, a[1001] = {}, m = 0;
+    int n, a[1001] = {}, m = 0, c;
     cin >> n;
     for(int i = 0; i < n; i++) cin >> a[i];
-    for(int i = 0; i < n-1; i++){
-        for(int j = i+1; j < n; j++) if(a[j] - a[i] > m) m = a[j] - a[i];
+
+    c = a[0];
+    for(int i = 1; i < n; i++){
+        if(a[i] - c > m) m = a[i] - c;
+        if(a[i] < c) c = a[i];
     }
     cout << m;
     return 0;

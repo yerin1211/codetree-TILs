1268b654043f92f66ff9429eae67b8c24040bc5b
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-08 16:54:22 +0900

    Title: 동전이 있는 위치, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240808/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, r, c, i, j;
+    cin >> n >> m;
+    int a[n][n] = {};
+    while(m--){
+        cin >> r >> c;
+        a[r-1][c-1] = 1;
+    }
+    for(i = 0; i < n; i++) for(j = 0; j < n; j++) cout << a[i][j] << (j - n + 1 ? ' ' : '\n');
+    return 0;
+}

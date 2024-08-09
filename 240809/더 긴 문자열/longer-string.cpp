23f2b19b5422eb986fab6677ad124842b5bdf43e
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-09 10:26:46 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240809/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+    if (a.length() > b.length()) cout << a << ' ' << a.length();
+    else if(a.length() < b.length()) cout << b << ' ' << b.length();
+    else cout << "same";
+    return 0;
+}

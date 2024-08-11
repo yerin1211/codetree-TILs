140f4ed91d3e7d3370d451f13911dc06e0859a66
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-11 23:16:03 +0900

    Title: 두 번째를 첫 번째로, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/두 번째를 첫 번째로/second-to-first.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a; char b, c;
+    cin >> a;
+    b = a[0];
+    c = a[1];
+    for(int i = 0; a[i] != '\0'; i++) if(a[i] == c) a[i] = b;
+    cout << a;
+    return 0;
+}

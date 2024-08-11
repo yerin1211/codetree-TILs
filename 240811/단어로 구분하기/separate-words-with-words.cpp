347d76a4d7abc6602a4775a57e77301422dffb9a
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-11 13:36:55 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240811/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a;
+    for(int i = 0; i < 10; i++) {
+        cin >> a;
+        cout << a << '\n';
+    }
+    return 0;
+}

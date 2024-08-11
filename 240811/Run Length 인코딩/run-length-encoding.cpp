c22d4eda5693e52646be817e796b4cf794f31243
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-11 21:30:07 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/Run Length 인코딩/run-length-encoding.cpp
+++ 240811/Run Length 인코딩/run-length-encoding.cpp
@@ -3,26 +3,18 @@
 using namespace std;
 
 int main() {
-    string a;
+    string a, s = "";
     cin >> a;
-    char b = a[0]; int c = a.length();
-    for(int i = 1; a[i] != '\0'; i++){
-        if(a[i] == b) c--;
-        else{
-            b = a[i];
-            c++;
-        } 
-    }
-    cout << c + 1 << '\n';
-    b = a[0]; c = 1;
+    char b = a[0]; int c = 1;
     for(int i = 1; a[i] != '\0'; i++){
         if(a[i] == b) c++;
         else{
-            cout << b << c;
+            s += b + to_string(c);
             c = 1;
             b = a[i];
         }
     }
-    cout << b << c;
+    s += b + to_string(c);
+    cout << s.length() << '\n' << s;
     return 0;
 }

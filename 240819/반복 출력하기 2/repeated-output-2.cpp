bbc1f02adfd623ba316efb991597a627e8a0df2f
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-19 09:53:35 +0900

    Title: 반복 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240819/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void f(int n){
+    if(n == 0) return;
+    cout << "HelloWorld\n";
+    f(n - 1);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    f(n);
+    return 0;
+}

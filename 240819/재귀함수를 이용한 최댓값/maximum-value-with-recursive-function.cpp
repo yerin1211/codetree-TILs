d05e7f1aca83fea669442b678021cb89acb6cb2e
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-19 18:01:22 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240819/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
+++ 240819/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int a[100];
 
-int f(int n, int m){
-    if(!n--) return m;
-    if(a[n] > m) return f(n, a[n]);
-    else return f(n, m);
+int f(int n){
+    if(!n--) return a[0];
+    return max(f(n), a[n]);
 }
 
 int main() {
     int n;
     cin >> n;
     for(int i = 0; i < n; i++) cin >> a[i];
-    cout << f(n, 0);
+    cout << f(n-1);
     return 0;
 }

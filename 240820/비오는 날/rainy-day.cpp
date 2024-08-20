6a41672845de979c3305864bce1eadadf3777a0f
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-20 19:03:22 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/비오는 날/rainy-day.cpp
+++ 240820/비오는 날/rainy-day.cpp
@@ -11,13 +11,13 @@ public:
 };
 
 int main() {
-    int n, i;
+    int n, i, j = -1;
     cin >> n;
     C c[n];
-    for(i = 0; i < n; i++){
+    for(i = 1; i < n; i++){
         cin >> c[i].a >> c[i].b >> c[i].c;
-        if(c[i].c == "Rain") break;
+        if(c[i].c == "Rain" && (j < 0 || c[i].a < c[j].a)) j = i;
     }
-    cout << c[i].a << ' ' << c[i].b << ' ' << c[i].c;
+    cout << c[j].a << ' ' << c[j].b << ' ' << c[j].c;
     return 0;
 }

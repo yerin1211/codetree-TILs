33799b1cb0377b001975d651659a3dc3b6a4f469
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-20 12:02:23 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240820/2개씩 그룹짓기/group-of-pairs.cpp
+++ 240820/2개씩 그룹짓기/group-of-pairs.cpp
@@ -3,22 +3,14 @@
 using namespace std;
 
 int main() {
-    int n, i, j, la, lb, m;
+    int n, i, m;
     cin >> n;
     int a[n*2];
     for(i = 0; i < n*2; i++) cin >> a[i];
     sort(a, a + n*2);
-    la = a[0];
-    lb = a[n*2-1];
-    m = la + lb;
+    m = a[0] + a[n*2-1];
     for(i = 1; i < n; i++){
-        if(la == a[i] && lb == a[2*n-1-i]){
-            for(j = 1; a[j] == a[i]; j++);
-            swap(a[i], a[j]);
-        }
         m = max(m, a[i] + a[2*n-1-i]);
-        la = a[i];
-        lb = a[2*n-1-i];
     }
     cout << m;
     return 0;

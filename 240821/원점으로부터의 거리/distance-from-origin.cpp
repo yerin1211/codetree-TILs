9204c1493d57e3b0404d4638392473e892d1a593
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-21 14:38:59 +0900

    Title: 원점으로부터의 거리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240821/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class C{
+public:
+    int a, b, c;
+    C(int a = 0, int b = 0){
+        this->a = a; this-> b = b;
+    }
+};
+
+bool cmp(const C &a, const C &b){
+    return abs(a.a) + abs(a.b) < abs(b.a) << abs(b.b);
+}
+
+int main() {
+    int n, i;
+    cin >> n;
+    C a[n];
+    for(i = 0; i < n; i++){
+        a[i].c = i + 1;
+        cin >> a[i].a >> a[i].b;
+    }
+    sort(a, a + n, cmp);
+    for(i = 0; i < n; i++) cout << a[i].c << '\n';
+    return 0;
+}

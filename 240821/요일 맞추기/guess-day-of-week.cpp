953b1b05aed101648b2bf30da1ae0d1505a796a2
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-21 17:59:45 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240821/요일 맞추기/guess-day-of-week.cpp
+++ 240821/요일 맞추기/guess-day-of-week.cpp
@@ -5,11 +5,15 @@ using namespace std;
 string y[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 int m[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
+int f(int a, int b){
+    for(int i = 1; i < a; i++) b += m[i];
+    return b;
+}
+
 int main() {
     int m1, d1, m2, d2;
     cin >> m1 >> d1 >> m2 >> d2;
-    d1 = d2 - d1 + 1;
-    for(; m1 < m2; m1++) d1 += m[m1];
+    d1 = f(m2, d2) - f(m1, d1) + 1;
     cout << y[d1 % 7];
     return 0;
 }

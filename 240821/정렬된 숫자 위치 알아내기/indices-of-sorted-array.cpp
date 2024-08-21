31ae5c82d3a898cea609260682a9baef9ca37743
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-21 15:16:49 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240821/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
+++ 240821/정렬된 숫자 위치 알아내기/indices-of-sorted-array.cpp
@@ -4,32 +4,27 @@ using namespace std;
 
 class C{
 public:
-    int a, b, c;
-    C(int a = 0, int b = 0, int c = 0){
+    int a, b;
+    C(int a = 0, int b = 0){
         this->a = a; this->b = b;
     }
 };
 
-bool cmp1(const C &a, const C &b){
+bool cmp(const C &a, const C &b){
     return (a.b - b.b ? a.b < b.b : a.a < b.a);
-}
-
-bool cmp2(const C &a, const C &b){
-    return a.a < b.a;
 }
 
 int main() {
     int n, i;
     cin >> n;
-    C a[n];
+    C a[n]; int ans[n];
     for(i = 0; i < n; i++){
         a[i].a = i + 1;
         cin >> a[i].b;
     }
-    sort(a, a + n, cmp1);
-    for(i = 0; i < n; i++) a[i].c = i + 1;
-    sort(a, a + n, cmp2);
-    for(i = 0; i < n; i++) cout << a[i].c << ' ';
+    sort(a, a + n, cmp);
+    for(i = 0; i < n; i++) ans[a[i].a - 1] = i + 1;
+    for(i = 0; i < n; i++) cout << ans[i] << ' ';
 
     return 0;
 }

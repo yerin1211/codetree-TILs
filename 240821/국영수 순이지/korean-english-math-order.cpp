a31e5fc93ef1d4cd854d7d90a63ea915f9a6654e
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-21 11:52:10 +0900

    Title: 국영수 순이지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240821/국영수 순이지/korean-english-math-order.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class C{
+public:
+    string d;
+    int a, b, c;
+    C(string d = "", int a = 0, int b = 0, int c = 0){
+        this->a - a; this->b = b; this->c = c; this->d = d;
+    }
+};
+
+bool cmp(const C &a, const C &b){
+    if(a.a != b.a) return a.a > b.a;
+    if(a.b != b.b) return a.b > b.b;
+    return a.c > b.c;
+}
+
+int main() {
+    int n, i;
+    cin >> n;
+    C a[n];
+    for(i = 0; i < n; i++) cin >> a[i].d >> a[i].a >> a[i].b >> a[i].c;
+    sort(a, a + n, cmp);
+    for(i = 0; i < n; i++) cout << a[i].d << ' ' << a[i].a << ' ' << a[i].b << ' ' << a[i].c << '\n';
+    return 0;
+}

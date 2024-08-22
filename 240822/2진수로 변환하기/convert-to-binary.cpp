ceefe598286728a139d52e633a60128fd4b14825
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-22 10:50:04 +0900

    Title: 2진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240822/2진수로 변환하기/convert-to-binary.cpp
+++ 240822/2진수로 변환하기/convert-to-binary.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     int n, a[20], i = 0;
     cin >> n;
-    while(n){
+    do{
         a[i++] = n % 2;
         n /= 2;
-    }
+    }while(n);
     for(--i; i >= 0; i--) cout << a[i];
     return 0;
 }

11fb160b6d4beaa560bd9771d0db21dac610665a
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-22 17:58:54 +0900

    Title: 선두를 지켜라, Time: 10ms, Memory: 7MB, Status: Wrong Answer - CodeTree

---
+++ 240822/선두를 지켜라/keep-the-lead.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, i = 1, a[1000001] = {}, b[1000001] = {}, v, t, c = 0, f = 0;
+    cin >> n >> m;
+
+    while(n--){
+        cin >> v >> t;
+        for(t += i; i < t; i++) a[i] = a[i-1] + v;
+    }
+
+    i = 1;
+    while(m--){
+        cin >> v >> t;
+        for(t += i; i < t; i++){
+            b[i] = b[i-1] + v;
+            if(!f && a[i] - b[i]) f = (a[1] > b[1] ? 1 : -1);
+            else if(f > 0 && a[i] < b[i] || f < 0 && a[i] > b[i]){
+                c++;
+                f *= -1;
+            }
+        }
+    }
+    cout << c;
+    return 0;
+}

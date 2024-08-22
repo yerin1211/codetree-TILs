1504bb41c931a1b872ea7238eaa35da26296fd32
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-22 15:35:39 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 19ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240822/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x1, y1, x2, y2, a[2001][2001] = {}, i, j;
+    cin >> x1 >> y1 >> x2 >> y2;
+    for(i = x1; i < x2; i++) for(j = y1; j < y2; j++) a[i+1000][j+1000] = 1;
+    cin >> x1 >> y1 >> x2 >> y2;
+    for(i = x1; i < x2; i++) for(j = y1; j < y2; j++) a[i+1000][j+1000] = 0;
+    x1 = y1 = 3000; 
+    x2 = y2 = -3000;
+    for(i = 0; i < 2001; i++){
+        for(j = 0; j < 2001; j++){
+            if(!a[i][j]) continue;
+            x1 = x1 < i ? x1 : i;
+            y1 = y1 < j ? y1 : j;
+            x2 = x2 > i ? x2 : i;
+            y2 = y2 > j ? y2 : j;
+        }
+    }
+    cout << (x2 - x1 + 1) * (y2 - y1 + 1);
+    return 0;
+}

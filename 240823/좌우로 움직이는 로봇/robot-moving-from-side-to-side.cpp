b3a2a08d10a5bc93e93cefbda46166927ae21c4b
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-23 10:49:54 +0900

    Title: 좌우로 움직이는 로봇, Time: 42ms, Memory: 7MB, Status: Passed - CodeTree

--- 240823/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 240823/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -9,25 +9,16 @@ int main() {
     i = 1; c = 0;
     while(n--){
         cin >> t >> d;
-        if(d == 'R') for(t += i; i < t; i++) a[i] = ++c;
-        else for(t += i; i < t; i++) a[i] = --c;
+        for(t += i; i < t; i++) a[i] = (d == 'R' ? ++c : --c);
     }
     while(i <= 1000000) a[i++] = c;
     
     i = 1; c = 0;
     while(m--){
         cin >> t >> d;
-        if(d == 'R'){
-            for(t += i; i < t; i++){
-                b[i] = ++c;
-                if(a[i] == b[i] && a[i-1] != b[i-1]) s++;
-            }
-        } 
-        else {
-            for(t += i; i < t; i++) {
-                b[i] = --c;
-                if(a[i] == b[i] && a[i-1] != b[i-1]) s++;
-            }
+        for(t += i; i < t; i++) {
+            b[i] = (d == 'R' ? ++c : --c);
+            if(a[i] == b[i] && a[i-1] != b[i-1]) s++;
         }
     }
     for(;i <= 1000000; i++) {

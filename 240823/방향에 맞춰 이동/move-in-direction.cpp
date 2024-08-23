f973afa11aeaf041e93449b73aa68f5c2a341bb2
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-23 12:10:34 +0900

    Title: 방향에 맞춰 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240823/방향에 맞춰 이동/move-in-direction.cpp
+++ 240823/방향에 맞춰 이동/move-in-direction.cpp
@@ -4,15 +4,16 @@ using namespace std;
 int dx[] = {-1, 0, 0, 1}, dy[] = {0, -1, 1, 0};
 
 int main() {
-    int n, x = 0, y = 0, b;
+    int n, x = 0, y = 0, b, d;
     char a;
     cin >> n;
     while(n--){
         cin >> a >> b;
-        if(a == 'W') x += dx[0]*b, y += dy[0]*b;
-        else if(a == 'S') x += dx[1]*b, y += dy[1]*b;
-        else if(a == 'N') x += dx[2]*b, y += dy[2]*b;
-        else x += dx[3]*b, y += dy[3]*b;
+        if(a == 'W') d = 0;
+        else if(a == 'S') d = 1;
+        else if(a == 'N') d = 2;
+        else d = 3;
+        x += dx[d]*b, y += dy[d]*b;
     }
     cout << x << ' ' << y;
     return 0;

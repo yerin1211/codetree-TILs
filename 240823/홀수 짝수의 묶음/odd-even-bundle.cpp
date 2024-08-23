d9327664b16d1b8ca63c119e006d922cd8f25b32
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-23 15:45:29 +0900

    Title: 홀수 짝수의 묶음, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240823/홀수 짝수의 묶음/odd-even-bundle.cpp
+++ 240823/홀수 짝수의 묶음/odd-even-bundle.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 int main() {
-    int n, i, a[1001], e = 0, o = 0, s = 0, f = 0;
+    int n, i, a[1000], e = 0, o = 0, s = 0;
     cin >> n;
     for(i = 0; i < n; i++){
         cin >> a[i];
         a[i] % 2 ? o++ : e++;
     }
     while(e || o){
-        if(f && o) o--; 
-        else if(f && !o) break;
+        if(s % 2 && o) o--; 
+        else if(s % 2) break;
         else if(e) e--;
         else if(o != 4) o -= 2;
         else o -= 4;
-        s++, f ^= 1;
+        s++;
     }
     cout << s;
     return 0;

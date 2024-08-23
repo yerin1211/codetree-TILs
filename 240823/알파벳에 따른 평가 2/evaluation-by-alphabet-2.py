2e38244370b75241f8d538211ca727a31bfb9d98
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-23 13:55:02 +0900

    Title: 알파벳에 따른 평가 2, Time: 125ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240823/알파벳에 따른 평가 2/evaluation-by-alphabet-2.py
@@ -0,0 +1,7 @@
+a = input()
+if(a == 'S'): print("Superior")
+elif(a == 'F'): print("Fantastic")
+elif(a == 'G'): print("Good")
+elif(a == 'U'): print("Usually")
+elif(a == 'E'): print("Effort")
+else: print("Failure")

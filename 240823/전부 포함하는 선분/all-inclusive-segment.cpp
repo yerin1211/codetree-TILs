8f12c59818abb190799c7540a67a01d3fc9023ab
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-23 18:27:22 +0900

    Title: 전부 포함하는 선분, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240823/전부 포함하는 선분/all-inclusive-segment.cpp
+++ 240823/전부 포함하는 선분/all-inclusive-segment.cpp
@@ -3,22 +3,19 @@
 using namespace std;
 
 int main() {
-    int n, i, ml = 0, mr = 0, l1, l2, r1, r2;
+    int n, i, j, l, r, m = 101;
     cin >> n;
     vector<vector<int>> a(n, vector<int>(2));
+    for(i = 0; i < n; i++) cin >> a[i][0] >> a[i][1];
     for(i = 0; i < n; i++){
-        cin >> a[i][0] >> a[i][1];
-        if(a[i][0] < a[ml][0]) ml = i;
-        if(a[i][1] > a[mr][0]) mr = i;
+        l = 101; r = 0;
+        for(j = 0; j < n; j++){
+            if(i == j) continue;
+            l = (a[j][0] < l ? a[j][0] : l);
+            r = (a[j][1] > r ? a[j][1] : r);
+        }
+        m = (r - l < m ? r - l : m);
     }
-    l1 = l2 = 101;
-    r1 = r2 = 0;
-    for(i = 0; i < n; i++){
-        if(ml != i && a[i][0] < l1) l1 = a[i][0];
-        if(mr != i && a[i][0] < l2) l2 = a[i][0];
-        if(ml != i && a[i][1] > r1) r1 = a[i][1];
-        if(mr != i && a[i][1] > r2) r2 = a[i][1];
-    }
-    cout << (r1 - l1 < r2 - l2 ? r1 - l1 : r2 - l2);
+    cout << m;
     return 0;
 }

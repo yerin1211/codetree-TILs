ef3ee50fa941e23d36b9b45d4ef897922827414a
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-23 18:17:58 +0900

    Title: 전부 포함하는 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240823/전부 포함하는 선분/all-inclusive-segment.cpp
+++ 240823/전부 포함하는 선분/all-inclusive-segment.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 using namespace std;
 
-bool cmp(vector<int> &a, vector<int> &b){
-    return a[1] > b[1];
-}
-
 int main() {
-    int n, i, l, m;
+    int n, i, ml = 0, mr = 0, l1, l2, r1, r2;
     cin >> n;
     vector<vector<int>> a(n, vector<int>(2));
-    for(i = 0; i < n; i++) cin >> a[i][0] >> a[i][1];
-    sort(a.begin(), a.end());
-    m = a[1][0] - a[0][0];
-    l = a[0][0];
-    sort(a.begin(), a.end(), cmp);
-    m = max(m, a[0][1] - a[1][1]);
-    cout << a[0][1] - l - m;
+    for(i = 0; i < n; i++){
+        cin >> a[i][0] >> a[i][1];
+        if(a[i][0] < a[ml][0]) ml = i;
+        if(a[i][1] > a[mr][0]) mr = i;
+    }
+    l1 = l2 = 101;
+    r1 = r2 = 0;
+    for(i = 0; i < n; i++){
+        if(a[i][0] < l2) l2 = a[i][0];
+        if(ml != i && a[i][0] < l1) l1 = a[i][0];
+        if(a[i][1] > r1) r1 = a[i][1];
+        if(mr != i && a[i][1] > r2) r2 = a[i][1];
+    }
+    cout << (r1 - l1 < r2 - l2 ? r1 - l1 : r2 - l2);
     return 0;
 }

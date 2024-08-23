99ba7d7d125034ded236f128c264acafdc31d633
Yerin <50609486+yerin1211@users.noreply.github.com>
2024-08-23 10:44:14 +0900

    Title: 좌우로 움직이는 로봇, Time: 5ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 240823/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m, a[1000001], b[1000001], i, t, c, s = 0;
+    char d;
+    cin >> n >> m;
+
+    i = 1; c = 0;
+    while(n--){
+        cin >> t >> d;
+        if(d == 'R') for(t += i; i < t; i++) a[i] = ++c;
+        else for(t += i; i < t; i++) a[i] = --c;
+    }
+    while(i <= 1000000) a[i++] = c;
+    
+    i = 1; c = 0;
+    while(m--){
+        cin >> t >> d;
+        if(d == 'R'){
+            for(t += i; i < t; i++){
+                b[i] = ++c;
+                if(a[i] == b[i] && a[i-1] != b[i-1]) s++;
+            }
+        } 
+        else {
+            for(t += i; i < t; i++) {
+                b[i] = --c;
+                if(a[i] == b[i] && a[i-1] != b[i-1]) s++;
+            }
+        }
+    }
+    cout << s;
+    return 0;
+}
